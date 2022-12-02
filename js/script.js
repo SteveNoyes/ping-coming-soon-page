93739dfeed1e1fe53c433f81a3d095383fe65d4b
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-12-01 21:30:40 -0500

    regex doc

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 
 
 function ValidateEmail(inputText) {
-
+  // regex pattern
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
   if(inputText.value.match(mailformat)) {
     console.log("Valid email address");
     return true;
   } else {
-    // alert("You have entered an invalid email address!");
     // add red border error class on wrong input 
     document.emailForm.emailInput.style.border = "1px solid red";
     return false;
