1f7b4d0fce5a2aada6febdc8887ad1401bbf926c
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-12-01 21:33:12 -0500

    docs

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ function ValidateEmail(inputText) {
   // regex pattern
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if(inputText.value.match(mailformat)) {
+    // remove before deployment
     console.log("Valid email address");
     return true;
   } else {
     // add red border error class on wrong input 
+    // temp
     document.emailForm.emailInput.style.border = "1px solid red";
     return false;
   }
