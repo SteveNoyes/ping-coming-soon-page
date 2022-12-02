d080439fcbfdd4b11d773c017b76499359125c4c
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-12-01 22:08:29 -0500

    function update

--- js/script.js
+++ js/script.js
@@ -1,46 +1,21 @@
 function ValidateEmail(inputText) {
+  // initialize
+  let field = document.getElementById("femail");
+  let error = document.getElementById("errorDiv");
   // regex pattern
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(inputText.value.match(mailformat)) {
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  // if input empty return Whoops message 
+  if(inputText.value.match(' ')) {
+    field.classList.add("err");
+    error.innerHTML = "Whoops! It looks like you forgot to add your email";
+    // else if match console.log 
+  } else if(inputText.value.match(mailformat)) {
+    field.classList.remove("err");
+    error.innerHTML = "";
     // remove before deployment
     console.log("Valid email address");
-    return true;
   } else {
-    // add red border error class on wrong input 
-    // temp
-    document.emailForm.emailInput.style.border = "1px solid red";
-    return false;
-  }
-}
-
-function check () {
-  // (C1) INIT
-  var valid = true, error = "", field = "";
-
-  // (C2) NAME
-  field = document.getElementById("fname");
-  error = document.getElementById("cname");
-  if (!field.checkValidity()) {
-    valid = false;
     field.classList.add("err");
-    error.innerHTML = "Name must be 2-4 characters\r\n";
-  } else {
-    field.classList.remove("err");
-    error.innerHTML = "";
+    error.innerHTML = "Please provide a valid email address";
   }
-
-  // (C3) NUMBER
-  field = document.getElementById("fnumber");
-  error = document.getElementById("cnumber");
-  if (!field.checkValidity()) {
-    valid = false;
-    field.classList.add("err");
-    error.innerHTML = "Num must be between 1-12\r\n";
-  } else {
-    field.classList.remove("err");
-    error.innerHTML = "";
-  }
-
-  // (C4) RESULT
-  return valid;
 }
